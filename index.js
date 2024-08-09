41e94fd91e25910ad7a7ceef17d95f42581dd380
Malick Roungou <mjbroungou@gmail.com>
2024-08-08 20:02:14 -0400

    efficiency updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const closeBtn = document.getElementById('close-btn');
 const openMenuBtn = document.getElementById('menu');
 const mobileNav = document.getElementById('overlay');
 const summaryOrder = document.getElementById('order-summary');
+const grindDiv = document.getElementById('grind');
 
 const options = document.querySelectorAll('input[type="radio"]');
 const expandBtn = document.getElementsByClassName('expand');
@@ -22,26 +23,23 @@ function updateSummary () {
     let resultFrequency 
 
     for (const option of coffee) {
-        if (option.id === 'capsule' && option.checked) {
-            disableGrind();
-        }else if (option.checked) {
-            enabled()
-        }
-        option.checked ? resultCoffee = option.value : null;
+       resultCoffee === 'capsule' ? disableGrind() : enableGrind();
+        // option.checked ? resultCoffee = option.value : null;
+        // resultCoffee = coffee?.find(option => option.checked)?.value ?? ''
+        resultCoffee = Array.from(coffee)?.find(option => option.checked)?.value ?? '';
     }
 
     for (const option of type) {
-        option.checked ? resultType = option.value : null;
+        resultType = Array.from(type)?.find(option => option.checked)?.value ?? ''
     }
     for (const option of quantity) {
-        option.checked ? resultQuantity = option.value : null;
-
+        resultQuantity = Array.from(quantity)?.find(option => option.checked)?.value ??''
     }
     for (const option of grindType) {
-        option.checked ? resultGrind = option.value : null;
+        resultGrind = Array.from(grindType)?.find(option => option.checked)?.value ?? ''
     }
     for (const option of frequency) {
-        option.checked ?  resultFrequency = option.value : null;
+        resultFrequency = Array.from(frequency)?.find(option => option.checked)?.value ?? ''
     }
 
     const summaryP = document.getElementById('test');
@@ -49,7 +47,7 @@ function updateSummary () {
 
     updatePrice(resultQuantity, resultFrequency)
 
-    summaryP.innerHTML = `
+    const summaryTemplate = `
      “I drink coffee ${coffee[0].checked ? 'using' : 'as' } <span class="opt-tkn">${resultCoffee}</span>,
                 with a <span class="opt-tkn" id="type">${resultType}</span> type of bean.
                 <span class="opt-tkn" id="quantity">${resultQuantity}g</span> ${!coffee[0].checked ? 'ground ala' : ''}
@@ -58,68 +56,43 @@ function updateSummary () {
     
         `
 
-    summaryModal.innerHTML = `
-     “I drink coffee ${coffee[0].checked ? 'using' : 'as' } <span class="opt-tkn">${resultCoffee}</span>,
-                with a <span class="opt-tkn" id="type">${resultType}</span> type of bean.
-                <span class="opt-tkn" id="quantity">${resultQuantity}g</span> ${!coffee[0].checked ? 'ground ala' : ''}
-                <span class="opt-tkn" id="grind">${resultGrind}</span>,
-                sent to me <span class="opt-tkn" id="frequency">every ${resultFrequency}</span>.”
-    
-        `
+    summaryP.innerHTML = summaryTemplate;
+    summaryModal.innerHTML = summaryTemplate;
 }
 
 function updatePrice (quantitySelected, freq) {
+
+    const prices = {
+        250: { week: 7.20, biWeekly: 9.60, month: 12.90},
+        500: { week: 13, biWeekly: 17.5, month: 22},
+        1000: { week: 22, biWeekly: 32, month: 42}
+    }
     const weekPrice = document.getElementById('week-price')
     const biWeeklyPrice = document.getElementById('2-weeks-price');
     const monthPrice = document.getElementById('month-price');
     const monthlyCostModal = document.getElementById('amt-total')
-    
 
     const dollars = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
     });
 
-    if (quantitySelected == 250) {
-        weekPrice.innerText = `${dollars.format(7.20)}`
-        biWeeklyPrice.innerText = `${dollars.format(9.60)}`
-        monthPrice.innerText = `${dollars.format(12.90)}`
-        if (freq == 'week') {
-            monthlyCostModal.innerHTML = `${dollars.format(7.20 * 4)}`
-        } else if (freq == '2 weeks') {
-            monthlyCostModal.innerHTML = `${dollars.format(9.60 * 2)}`
-        } else if (freq == 'month') {
-            monthlyCostModal.innerHTML = `${dollars.format(12)}`
-        }
-    } else if (quantitySelected == 500) {
-        weekPrice.innerText = `${dollars.format(13.0)}`
-        biWeeklyPrice.innerText = `${dollars.format(17.5)}`
-        monthPrice.innerText = `${dollars.format(22.0)}`
-        if (freq == 'week') {
-            monthlyCostModal.innerHTML = `${dollars.format(13 * 4)}`
-        } else if (freq == '2 weeks') {
-            monthlyCostModal.innerHTML = `${dollars.format(17.5 * 2)}`
-        } else if (freq == 'month') {
-            monthlyCostModal.innerHTML = `${dollars.format(22)}`
-        }
-    } else if (quantitySelected == 1000){ 
-        weekPrice.innerText = `${dollars.format(22.0)}`
-        biWeeklyPrice.innerText = `${dollars.format(32)}`
-        monthPrice.innerText = `${dollars.format(42)}`
-        if (freq == 'week') {
-            monthlyCostModal.innerHTML = `${dollars.format(22 * 4)}`
-        } else if (freq == '2 weeks') {
-            monthlyCostModal.innerHTML = `${dollars.format(32 * 2)}`
-        } else if (freq == 'month') {
-            monthlyCostModal.innerHTML = `${dollars.format(42)}`
-        }
+    const selectedPrice = prices[quantitySelected];
+    weekPrice.innerText = dollars.format(selectedPrice.week)
+    biWeeklyPrice.innerText = dollars.format(selectedPrice.biWeekly)
+    monthPrice.innerText = dollars.format(selectedPrice.month)
+
+    const monthlyPrices = {
+        'week': selectedPrice * 4,
+        '2 weeks': selectedPrice.biWeekly * 2,
+        'month': selectedPrice.month
     }
+
+    monthlyCostModal.innerText = dollars.format(monthlyPrices[freq])
 
 }
 
 const disableGrind = () => {
-    const grindDiv = document.getElementById('grind');
-    
     grindDiv.classList.add('disabled');
 
     grindType.forEach((option) => {
@@ -127,9 +100,7 @@ const disableGrind = () => {
     })
 }
 
-const enabled = () => {
-    const grindDiv = document.getElementById('grind');
-    
+const enableGrind = () => {   
     grindDiv.classList.remove('disabled');
 
     grindType.forEach((option) => {
@@ -137,9 +108,20 @@ const enabled = () => {
     })
 }
 
+function debounce(func, wait) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
+
+const debouncedUpdateOption = debounce(updateOption, 150);
 options.forEach((option) => {
-  option.addEventListener('change', updateOption)
-})
+    option.addEventListener('change', debouncedUpdateOption);
+});
+
+
 
 function updateOption() {
     for (const option of options) {
@@ -160,6 +142,14 @@ for (const btn of expandBtn) {
         } 
     })
 }
+
+function closeDialog(event) {
+    // If the target dialog is
+    if (!event.target.contains(summaryOrder)) return;
+    summaryOrder.close();
+  }
+
+document.addEventListener('click', closeDialog)
 
 
 const openModal = () => {
